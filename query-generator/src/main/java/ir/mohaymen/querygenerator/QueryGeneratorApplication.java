871a23cbfd2340b45763016933893abd2109c9e7
Mehrsa404 <me.hosseini306@gmail.com>
2026-09-12 11:15:09 +0330

    feat: initial commit

---
+++ query-generator/src/main/java/ir/mohaymen/querygenerator/QueryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.querygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryGeneratorApplication.class, args);
+    }
+
+}
---
+++ query-generator/src/test/java/ir/mohaymen/querygenerator/QueryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mohaymen.querygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
