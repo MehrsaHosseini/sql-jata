05adf8b61f303ce2171e3c099d14e8475ff8b3f0
Mehrsa404 <me.hosseini306@gmail.com>
2026-09-12 17:39:37 +0330

    fix: MultipleWhere

--- query-generator/src/main/java/ir/mohaymen/querygenerator/domain/where/MultipleWhere.java
+++ query-generator/src/main/java/ir/mohaymen/querygenerator/domain/where/MultipleWhere.java
@@ -3,6 +3,6 @@ package ir.mohaymen.querygenerator.domain.where;
 import java.util.List;
 
 public class MultipleWhere {
-    private List<BasicWhere> leftOperand;
+    private List<Where> leftOperand;
     private List<Where> rightOperand;
 }
