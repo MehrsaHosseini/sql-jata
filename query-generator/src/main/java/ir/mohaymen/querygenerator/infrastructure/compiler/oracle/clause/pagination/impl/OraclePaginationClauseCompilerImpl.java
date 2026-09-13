194b9b766374c4054dd3f5cb9abaa7a77ffe870a
Mehrsa404 <me.hosseini306@gmail.com>
2026-09-13 10:21:38 +0330

    feat: add pagination clause compiler

---
+++ query-generator/src/main/java/ir/mohaymen/querygenerator/infrastructure/compiler/oracle/clause/pagination/impl/OraclePaginationClauseCompilerImpl.java
@@ -0,0 +1,11 @@
+package ir.mohaymen.querygenerator.infrastructure.compiler.oracle.clause.pagination.impl;
+
+import ir.mohaymen.querygenerator.application.query.generate.common.model.QueryContext;
+import ir.mohaymen.querygenerator.infrastructure.compiler.oracle.clause.pagination.OraclePaginationClauseCompiler;
+
+public class OraclePaginationClauseCompilerImpl implements OraclePaginationClauseCompiler {
+    @Override
+    public QueryContext generatePaginationClause(QueryContext context) {
+        return null;
+    }
+}
